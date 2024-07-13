2f390f3732311e2d10f1301a9d1be69d60ff1fbc
Nartai <kanosadventure@gmail.com>
2024-07-13 14:30:16 +0600

    setup product

---
+++ ProductService/src/main/java/com/example/productservice/entity/Product.java
@@ -0,0 +1,22 @@
+package com.example.productservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(value = "products")
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
