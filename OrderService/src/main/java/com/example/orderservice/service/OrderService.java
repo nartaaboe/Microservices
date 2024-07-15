5a246f921e563d4c4b40b4e623d6a00d294520ef
Nartai <kanosadventure@gmail.com>
2024-07-15 16:58:02 +0600

    multiple instance find!

--- OrderService/src/main/java/com/example/orderservice/service/OrderService.java
+++ OrderService/src/main/java/com/example/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -34,8 +34,8 @@ public class OrderService {
         List<String> skuCodes =  order.getOrderItems().stream()
                 .map(OrderItem::getSkuCode)
                 .toList();
-        InventoryResponse[] inventoryResponses = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
