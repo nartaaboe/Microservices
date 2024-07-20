8f50034c08dce7458c2beb8edae1eab0a85d163a
Nartai <kanosadventure@gmail.com>
2024-07-20 21:40:08 +0600

    CircuitBreaker handling

--- OrderService/src/main/java/com/example/orderservice/service/OrderService.java
+++ OrderService/src/main/java/com/example/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder;
-    public void placeOrder(OrderRequest orderRequest) {
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         List<OrderItem> orderItems = orderRequest.getOrderItems()
@@ -43,6 +43,7 @@ public class OrderService {
         boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::isInStock);
         if(allProductsInStock){
             orderRepository.save(order);
+            return "Order placed";
         }
         else{
             throw new IllegalArgumentException("Product is not available");
