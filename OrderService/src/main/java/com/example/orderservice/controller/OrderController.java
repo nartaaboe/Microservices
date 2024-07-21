b4c455cc54eca0cc2ad979947961ec22a2538102
Nartai <kanosadventure@gmail.com>
2024-07-21 20:52:15 +0600

    logs optimized

--- OrderService/src/main/java/com/example/orderservice/controller/OrderController.java
+++ OrderService/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -2,9 +2,6 @@ package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.OrderRequest;
 import com.example.orderservice.service.OrderService;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
-import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
